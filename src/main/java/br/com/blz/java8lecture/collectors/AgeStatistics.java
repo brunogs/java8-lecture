ca8d0f2814b9225a2251b808c06c84eb49867203
Bruno Silva <brunogs.sp@gmail.com>
2017-06-03 01:53:01 -0300

    refactor: use collector

--- src/main/java/br/com/blz/java8lecture/collectors/AgeStatistics.java
+++ src/main/java/br/com/blz/java8lecture/collectors/AgeStatistics.java
@@ -6,22 +6,23 @@ import br.com.blz.java8lecture.domain.Customer;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 
+import static java.util.stream.Collectors.summarizingInt;
+
 public class AgeStatistics {
 
     public static void main(String[] args) {
 
         List<Customer> customers = CustomerDataset.customers(5000);
 
-        IntSummaryStatistics intSummaryStatistics = customers.stream()
-                .mapToInt(Customer::getAge)
-                .summaryStatistics();
+        IntSummaryStatistics ageSummary = customers.stream()
+                .collect(summarizingInt(Customer::getAge));
 
         System.out.println("customer ages statistics: ");
-        System.out.println("average: " + intSummaryStatistics.getAverage());
-        System.out.println("max: " + intSummaryStatistics.getMax());
-        System.out.println("min: " + intSummaryStatistics.getMin());
-        System.out.println("count: " + intSummaryStatistics.getCount());
-        System.out.println("sum: " + intSummaryStatistics.getSum());
+        System.out.println("average: " + ageSummary.getAverage());
+        System.out.println("max: " + ageSummary.getMax());
+        System.out.println("min: " + ageSummary.getMin());
+        System.out.println("count: " + ageSummary.getCount());
+        System.out.println("sum: " + ageSummary.getSum());
 
     }
 
