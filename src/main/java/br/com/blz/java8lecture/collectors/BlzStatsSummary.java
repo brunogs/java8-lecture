a63fcaef672acd954a5e557582aa6055208c2a53
Natan Deitch <natan.luz.deitch@gmail.com>
2017-06-06 23:37:34 -0300

    add: median list calculation

--- src/main/java/br/com/blz/java8lecture/collectors/BlzStatsSummary.java
+++ src/main/java/br/com/blz/java8lecture/collectors/BlzStatsSummary.java
@@ -1,34 +1,47 @@
 package br.com.blz.java8lecture.collectors;
 
 import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.DoubleStream;
 
 import static java.lang.Long.MAX_VALUE;
+import static java.util.Collections.emptyList;
 import static java.util.Comparator.naturalOrder;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.*;
 
-//TODO median
 public class BlzStatsSummary extends DoubleSummaryStatistics {
 
-    private List<Double> counter = new ArrayList<>();
+    private List<Double> items = new ArrayList<>();
 
     @Override
     public void accept(double value) {
-        counter.add(value);
+        items.add(value);
         super.accept(value);
     }
 
-    public Optional<List<Double>> getMode() {
+    public OptionalDouble getMedian() {
+        DoubleStream sortedValues = items.stream().mapToDouble(r -> r).sorted();
 
-        Map<Double, Long> collect = counter.stream().collect(groupingBy(identity(), counting()));
+        return items.size() % 2 == 0 ? evenSet(sortedValues) : oddSet(sortedValues);
+    }
 
-        if (collect.isEmpty()) return Optional.empty();
+    private OptionalDouble oddSet(DoubleStream sortedValues) {
+        return sortedValues.skip(items.size()/2).findFirst();
+    }
 
-        long max = collect.values().stream().max(naturalOrder()).orElse(MAX_VALUE);
+    private OptionalDouble evenSet(DoubleStream sortedValues) {
+        return sortedValues.skip((items.size() / 2) - 1).limit(2).average();
+    }
 
-        return Optional.of(collect.entrySet().stream()
-                .filter(e -> e.getValue() == max)
-                .map(Map.Entry::getKey)
-                .collect(toList()));
+    public List<Double> getMode() {
+
+        Map<Double, Long> valuesCounted = items.stream().collect(groupingBy(identity(), counting()));
+
+        if (valuesCounted.isEmpty()) return emptyList();
+
+        long max = valuesCounted.values().stream().max(naturalOrder()).orElse(MAX_VALUE);
+
+        return valuesCounted.entrySet().stream().filter(e -> e.getValue() == max).map(Entry::getKey).collect(toList());
     }
 }
--- src/test/java/br/com/blz/java8lecture/collectors/BlzStatsCollectorTest.java
+++ src/test/java/br/com/blz/java8lecture/collectors/BlzStatsCollectorTest.java
@@ -3,20 +3,42 @@ package br.com.blz.java8lecture.collectors;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Stream;
 
 import static br.com.blz.java8lecture.collectors.BlzStatsCollector.toBlzStatsCollector;
+import static org.junit.Assert.assertEquals;
 
 public class BlzStatsCollectorTest {
+
+    @Test
+    public void shouldGetAllMetricsAvailable() {
+        BlzStatsSummary blzStatsSummary = Stream.of(1d, 2d, 3d, 4d, 5d, 6d).collect(toBlzStatsCollector(Double::doubleValue));
+
+        List<Double> mode = blzStatsSummary.getMode();
+        OptionalDouble median = blzStatsSummary.getMedian();
+        double average = blzStatsSummary.getAverage();
+
+        Assert.assertFalse(mode.isEmpty());
+        Assert.assertTrue(median.isPresent());
+        Assert.assertNotEquals(0d, average, 0d);
+
+        assertEquals(6, mode.size());
+
+        assertEquals(3.5d, median.getAsDouble(), 0d);
+
+        assertEquals(3.5d, average, 0d);
+    }
 
     @Test
     public void shouldReturnModeForUniqueMode() {
 
         BlzStatsSummary blzStatsSummary = Stream.of(1d, 2d, 3d, 4d, 5d, 5d).collect(toBlzStatsCollector(Double::doubleValue));
 
-        Assert.assertTrue(blzStatsSummary.getMode().isPresent());
-        Assert.assertEquals(1, blzStatsSummary.getMode().get().size());
-        Assert.assertEquals(5d, blzStatsSummary.getMode().get().get(0), 0d);
+        Assert.assertFalse(blzStatsSummary.getMode().isEmpty());
+        assertEquals(1, blzStatsSummary.getMode().size());
+        assertEquals(5d, blzStatsSummary.getMode().get(0), 0d);
     }
 
     @Test
@@ -24,8 +46,8 @@ public class BlzStatsCollectorTest {
 
         BlzStatsSummary blzStatsSummary = Stream.of(1d, 1d, 2d, 3d, 4d, 4d, 5d, 5d).collect(toBlzStatsCollector(Double::doubleValue));
 
-        Assert.assertTrue(blzStatsSummary.getMode().isPresent());
-        Assert.assertEquals(3, blzStatsSummary.getMode().get().size());
+        Assert.assertFalse(blzStatsSummary.getMode().isEmpty());
+        assertEquals(3, blzStatsSummary.getMode().size());
     }
 
     @Test
@@ -33,6 +55,33 @@ public class BlzStatsCollectorTest {
 
         BlzStatsSummary blzStatsSummary = new BlzStatsSummary();
 
-        Assert.assertFalse(blzStatsSummary.getMode().isPresent());
+        Assert.assertTrue(blzStatsSummary.getMode().isEmpty());
+    }
+
+    @Test
+    public void shouldReturnMediaForOddList() {
+        BlzStatsSummary blzStatsSummary = Stream.of(1d, 1d, 2d, 3d, 4d, 4d, 5d, 5d, 6d).collect(toBlzStatsCollector(Double::doubleValue));
+
+        Assert.assertTrue(blzStatsSummary.getMedian().isPresent());
+        assertEquals(4d, blzStatsSummary.getMedian().getAsDouble(), 0d);
+
+    }
+
+    @Test
+    public void shouldReturnMediaForEvenList() {
+        BlzStatsSummary blzStatsSummary = Stream.of(1d, 1d, 2d, 3d, 5d, 4d, 5d, 5d, 6d, 6d).collect(toBlzStatsCollector(Double::doubleValue));
+
+        Assert.assertTrue(blzStatsSummary.getMedian().isPresent());
+        assertEquals(4.5d, blzStatsSummary.getMedian().getAsDouble(), 0d);
+
+    }
+
+    @Test
+    public void shouldReturnIdentityForOneItemList() {
+        BlzStatsSummary blzStatsSummary = Stream.of(6d).collect(toBlzStatsCollector(Double::doubleValue));
+
+        Assert.assertTrue(blzStatsSummary.getMedian().isPresent());
+        assertEquals(6d, blzStatsSummary.getMedian().getAsDouble(), 0d);
+
     }
 }
